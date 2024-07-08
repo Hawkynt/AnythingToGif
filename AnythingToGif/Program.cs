f1b13b24d9ee6ea8b021301cc2fc5f00dd8bf677
Hawkynt <github@synthelicz.de>
2024-07-08 15:01:22 +0200

    * refac

--- AnythingToGif/Program.cs
+++ AnythingToGif/Program.cs
@@ -11,13 +11,13 @@ class Program {
   static void Main() {
 
     ffmpeg.RootPath += "\\ffmpeg";
-    
+
     var examplesDirectory = new DirectoryInfo("Examples");
     foreach (var file in examplesDirectory.EnumerateFiles().Where(i => i.Extension.IsAnyOf(".avi", ".xvid", ".divx", ".mpg", ".mp2", ".mp4", ".mkv")))
       ProcessVideoFile(file);
     foreach (var file in examplesDirectory.EnumerateFiles().Where(i => i.Extension.IsAnyOf(".jpg", ".png", ".bmp", ".tif")))
       ProcessImageFile(file);
-    
+
     return;
 
     static void ProcessVideoFile(FileInfo inputFile) {
@@ -32,14 +32,14 @@ class Program {
         ColorOrdering = ColorOrderingMode.MostUsedFirst
       };
 
-      Dimensions? dimensions=null;
+      Dimensions? dimensions = null;
 
       var subImages = ProcessFrames();
       var enumerator = subImages.GetEnumerator();
       if (!enumerator.MoveNext())
         return;
 
-      if(dimensions!=null)
+      if (dimensions != null)
         _WriteGif(outputFile, dimensions.Value, Enumerate());
 
       enumerator?.Dispose();
@@ -49,18 +49,18 @@ class Program {
       IEnumerable<Frame> Enumerate() {
         do {
           yield return enumerator.Current;
-        }while(enumerator.MoveNext() ); 
+        } while (enumerator.MoveNext());
       }
 
       IEnumerable<Frame> ProcessFrames() {
         var i = 0;
         var durationDelta = TimeSpan.Zero; /* this is for keeping track how much we're behind actual timecodes in the video because of gif frame limits */
         foreach (var (image, duration) in FrameServer()) {
-          dimensions ??= new(image.Width,image.Height);
+          dimensions ??= new(image.Width, image.Height);
 
           converter.Quantizer = new OctreeQuantizer();
           converter.TotalFrameDuration = duration + durationDelta; /* if we're too early, keep displaying frames longer */
-          Console.WriteLine($"Processing frame {++i}");
+          Console.WriteLine($"Processing frame {++i}, on-screen for {duration.Milliseconds}ms, already off by {durationDelta.Milliseconds}ms");
 
           var currentDuration = TimeSpan.Zero;
           foreach (var frame in converter.Convert(image)) {
@@ -72,7 +72,7 @@ class Program {
         }
       }
 
-      IEnumerable<(Bitmap frame,TimeSpan duration)> FrameServer() {
+      IEnumerable<(Bitmap frame, TimeSpan duration)> FrameServer() {
         foreach (var image in VideoFrameExtractor.GetFrames(inputFile))
           yield return image;
       }
@@ -93,17 +93,17 @@ class Program {
 
       using var image = Image.FromFile(inputFile.FullName);
       using var bitmap = new Bitmap(image);
-      
+
       Console.WriteLine($"Converting {inputFile.Name}");
-      var subImages=converter.Convert(bitmap);
+      var subImages = converter.Convert(bitmap);
 
       _WriteGif(outputFile, (Dimensions)image.Size, subImages);
     }
 
 
-    static void _WriteGif(FileInfo file, Dimensions dimensions, IEnumerable<Frame> frames) 
+    static void _WriteGif(FileInfo file, Dimensions dimensions, IEnumerable<Frame> frames)
       => Writer.ToFile(file, dimensions, frames, LoopCount.NotSet, allowCompression: true)
-      ;
+    ;
 
   }
 }
--- AnythingToGif/VideoFrameExtractor.cs
+++ AnythingToGif/VideoFrameExtractor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -7,233 +8,291 @@ using System.Runtime.InteropServices;
 using FFmpeg.AutoGen;
 
 public static class VideoFrameExtractor {
+
   private unsafe struct FFmpegState : IDisposable {
-    private readonly AVFormatContext*[] pFormatContext = new AVFormatContext*[1];
-    private readonly AVCodecContext*[] pCodecContext = new AVCodecContext*[1];
-    private readonly AVFrame*[] pFrame = new AVFrame*[1];
-    private readonly AVFrame*[] pRgbFrame = new AVFrame*[1];
-    private readonly AVPacket*[] pPacket = new AVPacket*[1];
-    private readonly AVCodecParserContext*[] pParser = new AVCodecParserContext*[1];
-    private readonly SwsContext*[] pSwsContext = new SwsContext*[1];
-    private readonly AVStream* pStream;
-    private long previousPts = 0; 
-    private readonly IntPtr convertedFrameBufferPtr;
-    private byte_ptrArray4 dstData;
-    private int_array4 dstLinesize;
+
+    private const int STATUS_OK = 0;
+
+    private readonly List<Action> _disposals = new();
+
+    private readonly AVFormatContext*[] _pFormatContext = new AVFormatContext*[1];
+    private readonly AVCodecContext*[] _pCodecContext = new AVCodecContext*[1];
+    private readonly AVFrame*[] _pFrame = new AVFrame*[1];
+    private readonly AVFrame*[] _pRgbFrame = new AVFrame*[1];
+    private readonly AVPacket*[] _pPacket = new AVPacket*[1];
+    private readonly AVCodecParserContext*[] _pParser = new AVCodecParserContext*[1];
+    private readonly SwsContext*[] _pSwsContext = new SwsContext*[1];
+    private AVStream* _pStream;
+    private IntPtr _convertedFrameBufferPtr;
+    private byte_ptrArray4 _dstData;
+    private int_array4 _dstLinesize;
 
     public FFmpegState(FileInfo video) {
+      var self = this;
       ffmpeg.avdevice_register_all();
 
-      pFormatContext[0] = ffmpeg.avformat_alloc_context();
-      if (pFormatContext[0] == null)
+      // allocate memory for context
+      this._pFormatContext[0] = ffmpeg.avformat_alloc_context();
+      if (this._pFormatContext[0] == null)
         throw new ApplicationException("Could not allocate format context.");
+      
+      this._disposals.Add(() => {
+        ffmpeg.avformat_free_context(self._pFormatContext[0]);
+        self._pFormatContext[0] = null;
+      });
 
-      fixed (AVFormatContext** pFormatContextPtr = &pFormatContext[0]) {
-        if (ffmpeg.avformat_open_input(pFormatContextPtr, video.FullName, null, null) != 0)
+      // open format
+      fixed (AVFormatContext** pFormatContextPtr = &this._pFormatContext[0])
+        if (ffmpeg.avformat_open_input(pFormatContextPtr, video.FullName, null, null) != STATUS_OK)
           throw new ApplicationException("Could not open file.");
-      }
 
-      if (ffmpeg.avformat_find_stream_info(pFormatContext[0], null) != 0) {
-        Dispose();
+      this._disposals.Add(()=> {
+        fixed (AVFormatContext** pFormatContextPtr = &self._pFormatContext[0])
+          ffmpeg.avformat_close_input(pFormatContextPtr);
+
+        self._pFormatContext[0] = null;
+      });
+
+      // is there a stream in it?
+      if (ffmpeg.avformat_find_stream_info(this._pFormatContext[0], null) != STATUS_OK) {
+        this.Dispose();
         throw new ApplicationException("Could not find stream info.");
       }
 
-      for (var i = 0; i < pFormatContext[0]->nb_streams; i++) {
-        var avStream = pFormatContext[0]->streams[i];
-        if (avStream->codecpar->codec_type == AVMediaType.AVMEDIA_TYPE_VIDEO) {
-          pStream = avStream;
-          break;
-        }
+      // find the video stream
+      for (var i = 0; i < this._pFormatContext[0]->nb_streams; ++i) {
+        var avStream = this._pFormatContext[0]->streams[i];
+        if (avStream->codecpar->codec_type != AVMediaType.AVMEDIA_TYPE_VIDEO)
+          continue;
+
+        this._pStream = avStream;
+        break;
       }
 
-      if (pStream == null) {
-        Dispose();
+      if (this._pStream == null) {
+        this.Dispose();
         throw new ApplicationException("Could not find video stream.");
       }
 
-      var pCodec = ffmpeg.avcodec_find_decoder(pStream->codecpar->codec_id);
+      this._disposals.Add(() => self._pStream = null);
+
+      // find codec for the selected stream
+      var pCodec = ffmpeg.avcodec_find_decoder(this._pStream->codecpar->codec_id);
       if (pCodec == null) {
-        Dispose();
+        this.Dispose();
         throw new ApplicationException("Unsupported codec.");
       }
 
-      pCodecContext[0] = ffmpeg.avcodec_alloc_context3(pCodec);
-      if (pCodecContext[0] == null) {
-        Dispose();
+      // get memory to store a codec context
+      this._pCodecContext[0] = ffmpeg.avcodec_alloc_context3(pCodec);
+      if (this._pCodecContext[0] == null) {
+        this.Dispose();
         throw new ApplicationException("Could not allocate codec context.");
       }
 
-      if (ffmpeg.avcodec_parameters_to_context(pCodecContext[0], pStream->codecpar) < 0) {
-        Dispose();
+      this._disposals.Add(() => {
+        fixed(AVCodecContext** pCodecContextPtr = &self._pCodecContext[0])
+          ffmpeg.avcodec_free_context(pCodecContextPtr);
+
+        self._pCodecContext[0] = null;
+      });
+
+      // copy codec parameters to context
+      if (ffmpeg.avcodec_parameters_to_context(this._pCodecContext[0], this._pStream->codecpar) < STATUS_OK) {
+        this.Dispose();
         throw new ApplicationException("Could not copy codec parameters to context.");
       }
 
-      if (ffmpeg.avcodec_open2(pCodecContext[0], pCodec, null) < 0) {
-        Dispose();
+      // open codec
+      if (ffmpeg.avcodec_open2(this._pCodecContext[0], pCodec, null) < STATUS_OK) {
+        this.Dispose();
         throw new ApplicationException("Could not open codec.");
       }
 
-      pFrame[0] = ffmpeg.av_frame_alloc();
-      if (pFrame[0] == null) {
-        Dispose();
+      // allocate memory for frame
+      this._pFrame[0] = ffmpeg.av_frame_alloc();
+      if (this._pFrame[0] == null) {
+        this.Dispose();
         throw new ApplicationException("Could not allocate video frame.");
       }
 
-      pRgbFrame[0] = ffmpeg.av_frame_alloc();
-      if (pRgbFrame[0] == null) {
-        Dispose();
+      this._disposals.Add(()=> {
+        fixed (AVFrame** pFramePtr = &self._pFrame[0])
+          ffmpeg.av_frame_free(pFramePtr);
+        
+        self._pFrame[0] = null;
+      });
+
+      // allocate memory for rgb frame
+      this._pRgbFrame[0] = ffmpeg.av_frame_alloc();
+      if (this._pRgbFrame[0] == null) {
+        this.Dispose();
         throw new ApplicationException("Could not allocate RGB frame.");
       }
 
-      pPacket[0] = ffmpeg.av_packet_alloc();
-      if (pPacket[0] == null) {
-        Dispose();
+      this._disposals.Add(() => {
+        fixed (AVFrame** pRgbFramePtr = &self._pRgbFrame[0])
+          ffmpeg.av_frame_free(pRgbFramePtr);
+
+        self._pRgbFrame[0] = null;
+      });
+
+      // allocate memory for packet
+      this._pPacket[0] = ffmpeg.av_packet_alloc();
+      if (this._pPacket[0] == null) {
+        this.Dispose();
         throw new ApplicationException("Could not allocate packet.");
       }
 
-      pParser[0] = ffmpeg.av_parser_init((int)pCodec->id);
-      if (pParser[0] == null) {
-        Dispose();
+      this._disposals.Add(() => {
+        fixed (AVPacket** pPacketPtr = &self._pPacket[0])
+          ffmpeg.av_packet_free(pPacketPtr);
+
+        self._pPacket[0] = null;
+      });
+
+      // init parser
+      this._pParser[0] = ffmpeg.av_parser_init((int)pCodec->id);
+      if (this._pParser[0] == null) {
+        this.Dispose();
         throw new ApplicationException("Could not initialize parser.");
       }
 
-      pSwsContext[0] = ffmpeg.sws_getContext(
-          pCodecContext[0]->width,
-          pCodecContext[0]->height,
-          pCodecContext[0]->pix_fmt,
-          pCodecContext[0]->width,
-          pCodecContext[0]->height,
-          AVPixelFormat.AV_PIX_FMT_BGR24,
-          ffmpeg.SWS_BILINEAR,
-          null, null, null
+      this._disposals.Add(() => {
+        fixed (AVCodecParserContext** pParserPtr = &self._pParser[0])
+          ffmpeg.av_parser_close(*pParserPtr);
+
+        self._pParser[0] = null;
+      });
+
+      // init video2rgb converter
+      this._pSwsContext[0] = ffmpeg.sws_getContext(
+        this._pCodecContext[0]->width,
+        this._pCodecContext[0]->height,
+        this._pCodecContext[0]->pix_fmt,
+        this._pCodecContext[0]->width,
+        this._pCodecContext[0]->height,
+        AVPixelFormat.AV_PIX_FMT_BGR24,
+        ffmpeg.SWS_BILINEAR,
+        null,
+        null,
+        null
       );
 
-      if (pSwsContext[0] == null) {
-        Dispose();
+      if (this._pSwsContext[0] == null) {
+        this.Dispose();
         throw new ApplicationException("Could not initialize the conversion context.");
       }
 
-      int numBytes = ffmpeg.av_image_get_buffer_size(AVPixelFormat.AV_PIX_FMT_BGR24, pCodecContext[0]->width, pCodecContext[0]->height, 1);
-      convertedFrameBufferPtr = Marshal.AllocHGlobal(numBytes);
-      dstData = new() ;
-      dstLinesize = new ();
+      this._disposals.Add(() => {
+        ffmpeg.sws_freeContext(self._pSwsContext[0]);
+        self._pSwsContext[0] = null;
+      });
 
-      ffmpeg.av_image_fill_arrays(ref dstData, ref dstLinesize, (byte*)convertedFrameBufferPtr, AVPixelFormat.AV_PIX_FMT_BGR24, pCodecContext[0]->width, pCodecContext[0]->height, 1);
+      var numBytes = ffmpeg.av_image_get_buffer_size(AVPixelFormat.AV_PIX_FMT_BGR24, this._pCodecContext[0]->width, this._pCodecContext[0]->height, 1);
+      this._convertedFrameBufferPtr = Marshal.AllocHGlobal(numBytes);
+      this._disposals.Add(() => {
+        Marshal.FreeHGlobal(self._convertedFrameBufferPtr);
+        self._convertedFrameBufferPtr = IntPtr.Zero;
+      });
+
+      this._dstData = new();
+      this._dstLinesize = new();
+
+      ffmpeg.av_image_fill_arrays(ref this._dstData, ref this._dstLinesize, (byte*)this._convertedFrameBufferPtr, AVPixelFormat.AV_PIX_FMT_BGR24, this._pCodecContext[0]->width, this._pCodecContext[0]->height, 1);
     }
 
-    public bool TryGetNextFrame(out Bitmap? frame, out TimeSpan duration) {
-      frame = null;
+    private long _previousPts = 0;
+
+    public bool TryGetNextFrame([NotNullWhen(true)] out Bitmap? result, out TimeSpan duration) {
+      result = null;
       duration = TimeSpan.Zero;
 
-      fixed (AVPacket** packetPtr = &pPacket[0]) {
-        while (ffmpeg.av_read_frame(pFormatContext[0], *packetPtr) >= 0) {
-          if (pPacket[0]->stream_index == pStream->index) {
-            if (ffmpeg.avcodec_send_packet(pCodecContext[0], pPacket[0]) < 0) {
-              ffmpeg.av_packet_unref(pPacket[0]);
-              continue;
-            }
-
-            while (ffmpeg.avcodec_receive_frame(pCodecContext[0], pFrame[0]) == 0) {
-              ffmpeg.sws_scale(
-                  pSwsContext[0],
-                  pFrame[0]->data,
-                  pFrame[0]->linesize,
-                  0,
-                  pCodecContext[0]->height,
-                  this.dstData,
-                  this.dstLinesize
-              );
-
-              this.pRgbFrame[0]->data.UpdateFrom(this.dstData);
-              this.pRgbFrame[0]->linesize.UpdateFrom(this.dstLinesize);
-
-              var width = pCodecContext[0]->width;
-              var height = pCodecContext[0]->height;
-
-              frame = new Bitmap(width, height, PixelFormat.Format24bppRgb);
-              var bitmapData = frame.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, frame.PixelFormat);
-
-              var src = dstData[0];
-              var dst = (byte*)bitmapData.Scan0;
-              var srcStride = dstLinesize[0];
-              var dstStride = bitmapData.Stride;
-
-              if(srcStride==dstStride)
-                Buffer.MemoryCopy(src,dst,dstStride*height,srcStride*height);
-              else
-                for (var y = 0; y < height; y++) {
-                  Buffer.MemoryCopy(src, dst, dstStride, srcStride);
-                  src += srcStride;
-                  dst += dstStride;
-                }
-
-              frame.UnlockBits(bitmapData);
-
-              long pts = pPacket[0]->pts == ffmpeg.AV_NOPTS_VALUE ? previousPts + 1 : pPacket[0]->pts;
-              duration = TimeSpan.FromSeconds((pts - previousPts) * ffmpeg.av_q2d(pStream->time_base));
-              previousPts = pts;
-
-              ffmpeg.av_packet_unref(pPacket[0]);
-              return true;
-            }
+      var ptsFactor = ffmpeg.av_q2d(this._pStream->time_base);
+      fixed (AVPacket** packetPtr = &this._pPacket[0])
+        while (ffmpeg.av_read_frame(this._pFormatContext[0], *packetPtr) >= FFmpegState.STATUS_OK) {
+          
+          // was it a frame for our video stream?
+          if (this._pPacket[0]->stream_index != this._pStream->index) {
+            ffmpeg.av_packet_unref(this._pPacket[0]);
+            continue;
           }
 
-          ffmpeg.av_packet_unref(pPacket[0]);
+          // can we send the frame to our decoder?
+          if (ffmpeg.avcodec_send_packet(this._pCodecContext[0], this._pPacket[0]) < FFmpegState.STATUS_OK) {
+            ffmpeg.av_packet_unref(this._pPacket[0]);
+            continue;
+          }
+
+          // can he decode it?
+          if (ffmpeg.avcodec_receive_frame(this._pCodecContext[0], this._pFrame[0]) != FFmpegState.STATUS_OK) {
+            ffmpeg.av_packet_unref(this._pPacket[0]);
+            continue;
+          }
+
+          // convert to RGB
+          ffmpeg.sws_scale(
+            this._pSwsContext[0],
+            this._pFrame[0]->data,
+            this._pFrame[0]->linesize,
+            0,
+            this._pCodecContext[0]->height,
+            this._dstData,
+            this._dstLinesize
+          );
+
+          this._pRgbFrame[0]->data.UpdateFrom(this._dstData);
+          this._pRgbFrame[0]->linesize.UpdateFrom(this._dstLinesize);
+
+          var width = this._pCodecContext[0]->width;
+          var height = this._pCodecContext[0]->height;
+
+          result = ConvertToBitmap(width, height, this._dstData[0], this._dstLinesize[0]);
+
+          var pts = this._pPacket[0]->pts == ffmpeg.AV_NOPTS_VALUE ? this._previousPts + 1 : this._pPacket[0]->pts;
+          duration = TimeSpan.FromSeconds((pts - this._previousPts) * ptsFactor);
+          this._previousPts = pts;
+
+          ffmpeg.av_packet_unref(this._pPacket[0]);
+          return true;
         }
-      }
 
       return false;
+
+      Bitmap ConvertToBitmap(int width, int height, byte* srcFrame, int srcStride) {
+        var result = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+        BitmapData? bitmapData = null;
+        try {
+          bitmapData = result.LockBits(new(0, 0, width, height), ImageLockMode.WriteOnly, result.PixelFormat);
+
+          var dst = (byte*)bitmapData.Scan0;
+          var dstStride = bitmapData.Stride;
+
+          if (srcStride == dstStride)
+            Buffer.MemoryCopy(srcFrame, dst, dstStride * height, srcStride * height);
+          else
+            for (var y = 0; y < height; ++y) {
+              Buffer.MemoryCopy(srcFrame, dst, dstStride, srcStride);
+              srcFrame += srcStride;
+              dst += dstStride;
+            }
+        } finally {
+          if (bitmapData != null)
+            result.UnlockBits(bitmapData);
+        }
+
+        return result;
+      }
+
     }
 
     public void Dispose() {
-      if (pParser[0] != null) {
-        fixed (AVCodecParserContext** pParserPtr = &pParser[0]) {
-          ffmpeg.av_parser_close(*pParserPtr);
-        }
-        pParser[0] = null;
-      }
-
-      if (pCodecContext[0] != null) {
-        fixed (AVCodecContext** pCodecContextPtr = &pCodecContext[0]) {
-          ffmpeg.avcodec_free_context(pCodecContextPtr);
-        }
-        pCodecContext[0] = null;
-      }
-
-      if (pFrame[0] != null) {
-        fixed (AVFrame** pFramePtr = &pFrame[0]) {
-          ffmpeg.av_frame_free(pFramePtr);
-        }
-        pFrame[0] = null;
-      }
-
-      if (pRgbFrame[0] != null) {
-        fixed (AVFrame** pRgbFramePtr = &pRgbFrame[0]) {
-          ffmpeg.av_frame_free(pRgbFramePtr);
-        }
-        pRgbFrame[0] = null;
-      }
-
-      if (pPacket[0] != null) {
-        fixed (AVPacket** pPacketPtr = &pPacket[0]) {
-          ffmpeg.av_packet_free(pPacketPtr);
-        }
-        pPacket[0] = null;
-      }
-
-      if (pFormatContext[0] != null) {
-        fixed (AVFormatContext** pFormatContextPtr = &pFormatContext[0]) {
-          ffmpeg.avformat_close_input(pFormatContextPtr);
-        }
-        pFormatContext[0] = null;
-      }
-
-      if (pSwsContext[0] != null) {
-        ffmpeg.sws_freeContext(pSwsContext[0]);
-        pSwsContext[0] = null;
+      for (var i = this._disposals.Count - 1; i >= 0; --i) {
+        this._disposals[i]();
+        this._disposals.RemoveAt(i);
       }
     }
-    
+
   }
 
   public static IEnumerable<(Bitmap frame, TimeSpan duration)> GetFrames(FileInfo video) {
@@ -241,4 +300,5 @@ public static class VideoFrameExtractor {
     while (state.TryGetNextFrame(out var frame, out var duration))
       yield return (frame, duration);
   }
+
 }
