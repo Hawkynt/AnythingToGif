31f8eeb2d6f1edae1eaf9e58b798fdfafa94f698
Hawkynt <github@synthelicz.de>
2024-07-10 11:07:48 +0200

    + prepared multi-dim frames

--- Gif/Dimensions.cs
+++ Gif/Dimensions.cs
@@ -6,6 +6,8 @@ using System;
 
 public readonly record struct Dimensions(ushort Width, ushort Height) {
 
+  public static Dimensions Empty { get; } = new(0, 0);
+
   public Dimensions(int width, int height) : this((ushort)width, (ushort)height) {
     ArgumentOutOfRangeException.ThrowIfNegative(width);
     ArgumentOutOfRangeException.ThrowIfNegative(height);
@@ -16,5 +18,7 @@ public readonly record struct Dimensions(ushort Width, ushort Height) {
   public static Dimensions FromSize(Size size) => new(size.Width, size.Height);
 
   public static explicit operator Dimensions(Size size) => Dimensions.FromSize(size);
+
+  public Dimensions With(Dimensions other) => new(Math.Max(this.Width, other.Width), Math.Max(this.Height, other.Height));
 
 }
