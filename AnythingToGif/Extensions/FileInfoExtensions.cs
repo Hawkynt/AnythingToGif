2bc253478284e57064a7b2f7934c834dea8625ec
Hawkynt <github@synthelicz.de>
2024-07-10 10:06:33 +0200

    + support more input formats

--- AnythingToGif/Extensions/FileInfoExtensions.cs
+++ AnythingToGif/Extensions/FileInfoExtensions.cs
@@ -6,7 +6,12 @@ using System.IO;
 
 internal static partial class FileInfoExtensions {
 
-  public static bool LooksLikeVideo(this FileInfo @this) => @this.Extension.IsAnyOf(StringComparison.OrdinalIgnoreCase, ".xvid", ".divx", ".mpg", ".mp2", ".mp4", ".mkv");
-  public static bool LooksLikeImage(this FileInfo @this) => @this.Extension.IsAnyOf(StringComparison.OrdinalIgnoreCase, ".jpg", ".png", ".bmp", ".tif");
+  public static bool LooksLikeVideo(this FileInfo @this) => @this.Extension.IsAnyOf(StringComparison.OrdinalIgnoreCase,
+    ".xvid", ".divx", ".mpg", ".mpeg", ".mp2", ".mp4", ".m4v", ".mkv", ".mov", ".avi", ".wmv", ".flv", ".f4v", ".webm", ".3gp", ".3g2", ".vob", ".ogv", ".ts", ".m2ts", ".mts", ".mxf")
+  ;
+
+  public static bool LooksLikeImage(this FileInfo @this) => @this.Extension.IsAnyOf(StringComparison.OrdinalIgnoreCase,
+    ".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff", ".gif", ".ico")
+  ;
 
 }
